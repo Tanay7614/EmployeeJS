68c62d8902b891ab83989fcfe0a6c26b6f6df3a9
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-11-26 20:16:35 +0530

    Calculate Daily Wage

---
+++ Employee.js
@@ -0,0 +1,20 @@
+const IS_FULL_TIME = 1;
+const IS_PART_TIME = 2;
+const WAGE_PER_HOURS = 20;
+
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+switch(empCheck){
+    case IS_FULL_TIME:
+        empHrs = 8;
+        break;
+    case IS_PART_TIME:
+        empHrs = 4;
+        break;
+    default:
+        empHrs = 0;
+}
+
+let empWage = empHrs * WAGE_PER_HOURS;
+console.log("Employee Wage: " + empWage);
+
